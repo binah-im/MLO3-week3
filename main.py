8f50c4793aaa0fd1b1c0533fed618858bbcd2d4b
Yoni <yonihkmail@gmail.com>
2022-04-26 22:32:11 +0300

    updated with Bonus

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"message": "Hello AWS World!"}
+
+
+#Bonus
+#import logging
+#from logging.config import dictConfig
+#from log_config import log_config               # my local file
+#Bonus dictConfig(log_config)
+#Bonus logger = logging.getLogger("my-project-logger") # should be this name unless you change it in log_config.py
